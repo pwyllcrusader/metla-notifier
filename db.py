ff36023229ee6f2534fc53dce6b2d2d89ecf0417
pwyllcrusader <pwyllcrusader@gmail.com>
2021-08-03 15:14:03 +0300

    init

---
+++ db.py
@@ -0,0 +1,10 @@
+def create_db():
+    ...
+
+
+def add_release_to_db(release):
+    ...
+
+
+def get_releases_from_db():
+    ...
---
+++ metalarea.py
@@ -0,0 +1,18 @@
+def login(username, password):
+    ...
+
+
+def go_to_releases_tab():
+    ...
+
+
+def switch_to_hot_releases_view():
+    ...
+
+
+def get_release_links():
+    ...
+
+
+def parse_release():
+    ...
